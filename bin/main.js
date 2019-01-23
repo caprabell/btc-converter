55b14f33014dd87efb78b2051bbb200807f63158
Mauricio Bell <mauriciocbell@gmail.com>
2019-01-23 13:24:09 -0200

    Delete main.js

--- bin/main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var program = require('commander');
-var pkg = require('../package.json');
-
-program.version(pkg.version).description('Convert Bitcoin to any provided currency').parse(process.argv);
